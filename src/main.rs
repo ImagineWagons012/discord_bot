6a31310e159f4c823b2e4e799df0efe76e42afd7
Paless Domonkos <palessdomi@gmail.com>
2023-03-22 11:00:39 +0100

    adsad

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::fs::File;
+use std::io::Read;
 use serenity::async_trait;
 use serenity::prelude::*;
 use serenity::model::channel::Message;
@@ -14,14 +16,17 @@ struct Handler;
 impl EventHandler for Handler {}
 
 #[tokio::main]
-async fn main() {
+async fn main() -> std::io::Result<()>{
     let framework = StandardFramework::new()
         .configure(|c| c.prefix("%")) // set the bot's prefix to "~"
         .group(&GENERAL_GROUP);
 
     // Login with a bot token from the environment
+    let mut file = File::open("token.txt")?;
+    let mut token = "".to_string();
+    file.read_to_string(&mut token)?;
     let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
-    let mut client = Client::builder("token", intents)
+    let mut client = Client::builder(token, intents)
         .event_handler(Handler)
         .framework(framework)
         .await
@@ -31,11 +36,19 @@ async fn main() {
     if let Err(why) = client.start().await {
         println!("An error occurred while running the client: {:?}", why);
     }
+    Ok(())
 }
 
 #[command]
 async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
     msg.reply(ctx, "Pong!").await?;
+
+    Ok(())
+}
+
+#[command]
+async fn yoke(ctx: &Context, msg: &Message) -> CommandResult {
+    msg.reply(ctx, "Dezz nuts!").await?;
 
     Ok(())
 }
